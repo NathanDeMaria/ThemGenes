76a54b6965d0ec59bf368555fb8cd58fb7c18f91
NathanDeMaria <naterstd@gmail.com>
2014-09-26 18:26:43 -0500

    Function for creating initial generation

--- R/algorithm.R
+++ R/algorithm.R
@@ -1,3 +1,22 @@
+require(data.table)
 
 # Implement genetic algorithm
 
+# create initial generation ####
+create_initial_gen <- function(count, init_means = 0, init_sd = 1) {
+	
+	initial_gen <- data.table(w = rnorm(count, init_means, init_sd), 
+							  x = rnorm(count, init_means, init_sd), 
+							  y = rnorm(count, init_means, init_sd), 
+							  z = rnorm(count, init_means, init_sd))
+	
+	initial_gen
+}
+
+# check fitness
+
+
+# have babies
+
+
+# die
--- tests/test_algorithm.R
+++ tests/test_algorithm.R
@@ -1,3 +1,11 @@
 
 source('../R/algorithm.R')
 context('Initial test')
+
+test_that('Initial gen works', {
+	
+	result <- create_initial_gen(10)
+	
+	expect_is(result, 'data.table')
+	expect_equal(nrow(result), 10)	
+})
