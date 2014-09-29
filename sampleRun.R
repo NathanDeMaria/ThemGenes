f9309337bc4182dbbe01dc085e4378e9676a80b5
NathanDeMaria <naterstd@gmail.com>
2014-09-28 22:44:00 -0500

    Recording errors into a data.table

--- sampleRun.R
+++ sampleRun.R
@@ -4,9 +4,9 @@ source('R/objective.R')
 source('Helpers/progressBar.R')
 
 set.seed(18)
-generation <- create_initial_gen(100)
+generation <- create_initial_gen(50)
 
-init_errors <- generation[, check_fitness(w,x,y,z)]
+errors <- data.table(generation=0, error=generation[, check_fitness(w,x,y,z)])
 
 iterations <- 2e3
 
@@ -15,10 +15,11 @@ bar$init(length = iterations, title = 'Generations')
 
 for(i in 1:iterations) {
 	
-	generation <- next_generation(generation, 15, 15, baby_sd = 2)
+	generation <- next_generation(generation, 5, 5, baby_sd = 3)
+	errors <- rbindlist(list(errors, data.table(generation = i, error = generation[,check_fitness(w,x,y,z)])))
 	bar$inc()
 }
 bar$close()
-end_errors <- generation[, check_fitness(w,x,y,z)]
-plot(init_errors, ylim=c(0, 70))
-points(end_errors, pch=19)
+
+g <- ggplot(errors[error < 145]) + geom_point(aes(generation, error))
+plot(g)
